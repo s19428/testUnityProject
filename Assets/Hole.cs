4f128ea29700fc8ab9b23d9142c18c3e7f9d24c3
s19428 <s19428@LAB132-07>
2020-10-14 18:56:05 +0200

    first

---
+++ Assets/Hole.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hole : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 600.0f;
+    public float jumpForce = 1;
+    public Rigidbody2D rb;
+
+    public float xDisplacement;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce));
+        isGrounded = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+}
